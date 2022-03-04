9aa21358a518c313f2680967f0dc2ab70279cfef
Felamande <elweinfancon@gmail.com>
2022-03-04 21:44:32 +0800

    add qml

--- 1.go
+++ 1.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"runtime"
 	"strings"
-	"time"
 
 	"github.com/Felamande/go-socks5"
 	"github.com/Felamande/qproxy/socks5server"
@@ -17,151 +16,169 @@ import (
 var verTag string
 var verCommitHash string
 
-type HideWindow struct {
-	widgets.QMainWindow
+type Socks5ServerGroupBox struct {
+	widgets.QGroupBox
+
+	portLabel     *widgets.QLabel
+	portLineInput *widgets.QLineEdit
+	startButton   *widgets.QPushButton
+	stopButton    *widgets.QPushButton
+	infoLineEdit  *widgets.QLineEdit
+
+	s5s *socks5server.Socks5Server
+
+	layout *widgets.QGridLayout
 }
 
-func (w *HideWindow) CloseEvent(event gui.QCloseEvent_ITF) {
-	w.Hide()
-	event.QCloseEvent_PTR().Ignore()
+func (gb *Socks5ServerGroupBox) Init() *Socks5ServerGroupBox {
+
+	gb.portLabel = widgets.NewQLabel2("端口:", nil, 0)
+	gb.portLineInput = widgets.NewQLineEdit2("33899", nil)
+	gb.startButton = widgets.NewQPushButton2("开始", nil)
+	gb.stopButton = widgets.NewQPushButton2("结束", nil)
+	gb.infoLineEdit = widgets.NewQLineEdit(nil)
+
+	expandSizePol := widgets.NewQSizePolicy()
+	expandSizePol.SetHorizontalPolicy(widgets.QSizePolicy__Expanding)
+	gb.startButton.SetSizePolicy(expandSizePol)
+	gb.stopButton.SetSizePolicy(expandSizePol)
+
+	gb.stopButton.SetEnabled(false)
+	gb.infoLineEdit.SetReadOnly(true)
+
+	gb.portLineInput.SetValidator(gui.NewQIntValidator2(0, 65535, gb.portLineInput))
+
+	gb.layout = widgets.NewQGridLayout2()
+	gb.layout.AddWidget2(gb.portLabel, 0, 0, 0)
+	gb.layout.AddWidget2(gb.portLineInput, 0, 1, 0)
+	gb.layout.AddWidget2(gb.startButton, 1, 0, 0)
+	gb.layout.AddWidget2(gb.stopButton, 1, 1, 0)
+	gb.layout.AddWidget2(gb.infoLineEdit, 2, 0, 0)
+	gb.SetLayout(gb.layout)
+
+	gb.s5s = socks5server.NewSocks5Server(nil).Init()
+
+	gb.startButton.ConnectClicked(func(checked bool) {
+		gb.s5s.StartServer(gb.portLineInput.Text())
+	})
+	gb.stopButton.ConnectClicked(func(checked bool) {
+		gb.s5s.StopServer()
+	})
+
+	gb.s5s.ConnectRunStateChange(gb.runStateChange)
+	gb.s5s.ConnectReceiveRunningError(gb.processRunningError)
+
+	return gb
+
 }
 
-func (w *HideWindow) ChangeEvent(event core.QEvent_ITF) {
-	if event.QEvent_PTR().Type() == core.QEvent__WindowStateChange && w.IsMinimized() {
-		w.Hide()
-		event.QEvent_PTR().Ignore()
+func (gb *Socks5ServerGroupBox) processRunningError(iErr interface{}) {
+
+	switch err := iErr.(type) {
+	case socks5.ListenError:
+		gb.infoLineEdit.SetText(err.Error())
+	default:
+		return
 	}
+}
+
+func (gb *Socks5ServerGroupBox) runStateChange(isRunning bool) {
+	gb.startButton.SetEnabled(!isRunning)
+	gb.stopButton.SetEnabled(isRunning)
+	gb.portLineInput.SetEnabled(!isRunning)
+}
+
+type ProxyAppWidget struct {
+	widgets.QWidget
+
+	s5sGroupBox *Socks5ServerGroupBox
+	layout      *widgets.QGridLayout
+}
+
+func (w *ProxyAppWidget) Init() *ProxyAppWidget {
+	w.s5sGroupBox = NewSocks5ServerGroupBox(nil).Init()
+
+	w.layout = widgets.NewQGridLayout(nil)
+	w.layout.AddWidget2(w.s5sGroupBox, 1, 0, 0)
+	w.SetLayout(w.layout)
+
+	return w
+}
+
+type ProxyAppTray struct {
+	widgets.QSystemTrayIcon
+
+	menu    *widgets.QMenu
+	actions map[string]*widgets.QAction
+}
+
+func (t *ProxyAppTray) Init() *ProxyAppTray {
+	t.actions = make(map[string]*widgets.QAction)
+	t.menu = widgets.NewQMenu(nil)
+	t.SetContextMenu(t.menu)
+
+	return t
+}
+
+func (t *ProxyAppTray) AddAction(text string, triggerFn func(bool)) *ProxyAppTray {
+	action := t.menu.AddAction(text)
+
+	action.ConnectTriggered(triggerFn)
+	t.actions[text] = action
+
+	return t
+}
+
+type ProxyAppWindow struct {
+	widgets.QMainWindow
+
+	appWidget *ProxyAppWidget
+
+	tray *ProxyAppTray
+}
+
+func (w *ProxyAppWindow) Init() *ProxyAppWindow {
+	w.appWidget = NewProxyAppWidget(w, 0).Init()
+	w.SetCentralWidget(w.appWidget)
+
+	w.tray = NewProxyAppTray(nil).Init()
+	w.tray.SetIcon(gui.NewQIcon5(":/qml/icon.ico"))
+
+	w.tray.AddAction("打开界面", func(bool) {
+		w.Show()
+	}).AddAction("关闭程序", func(bool) {
+		gui.QGuiApplication_SetQuitOnLastWindowClosed(true)
+		w.tray.Hide()
+		core.QCoreApplication_Instance().Quit()
+	}).AddAction("关于程序", func(b bool) {
+		widgets.QMessageBox_About(nil, "about qproxy", fmt.Sprintf("qproxy %s(%s)\nQt %s\n%s", verTag, verCommitHash, core.QtGlobal_qVersion(), strings.Replace(runtime.Version(), "go", "Go", -1)))
+	})
+
+	w.tray.ConnectActivated(func(reason widgets.QSystemTrayIcon__ActivationReason) {
+		switch reason {
+		case widgets.QSystemTrayIcon__DoubleClick:
+			w.Show()
+		}
+	})
+
+	return w
+}
+
+func (w *ProxyAppWindow) ShowTray() {
+	w.tray.Show()
 }
 
 func main() {
 	app := widgets.NewQApplication(len(os.Args), os.Args)
 
-	sserver := socks5server.NewSocks5Server()
-
-	var (
-		validatorGroup    = widgets.NewQGroupBox2("socks5代理服务器", nil)
-		validatorLabel    = widgets.NewQLabel2("端口:", nil, 0)
-		validatorLineEdit = widgets.NewQLineEdit(nil)
-		StartButton       = widgets.NewQPushButton(nil)
-		StopButton        = widgets.NewQPushButton(nil)
-		infoLineEdit      = widgets.NewQLineEdit(nil)
-		aboutButton       = widgets.NewQPushButton(nil)
-
-		sysTray = widgets.NewQSystemTrayIcon(nil)
-	)
-
-	sysTray.SetIcon(gui.NewQIcon5(":/qml/icon.ico"))
-
-	systrayMenu := widgets.NewQMenu(nil)
-	openWinAction := systrayMenu.AddAction("打开界面")
-	closeWinAction := systrayMenu.AddAction("关闭程序")
-
-	sysTray.Show()
-
-	expandSizePol := widgets.NewQSizePolicy()
-	expandSizePol.SetHorizontalPolicy(widgets.QSizePolicy__Expanding)
-
-	StartButton.SetSizePolicy(expandSizePol)
-	StopButton.SetSizePolicy(expandSizePol)
-	aboutButton.SetSizePolicy(expandSizePol)
-
-	StartButton.SetText("开始")
-	StopButton.SetText("结束")
-	aboutButton.SetText("关于")
-
-	StopButton.SetEnabled(false)
-	infoLineEdit.SetReadOnly(true)
-
-	validatorLineEdit.SetValidator(gui.NewQIntValidator2(0, 65535, validatorLineEdit))
-	validatorLineEdit.SetText("33899")
-
-	infoLineEdit.SetSizePolicy(expandSizePol)
-
-	StartButton.ConnectClicked(func(bool) {
-
-		sserver.Start(validatorLineEdit.Text())
-		infoLineEdit.SetText("start: " + validatorLineEdit.Text())
-		isRunning := sserver.GetRunState()
-		StartButton.SetEnabled(!isRunning)
-		StopButton.SetEnabled(isRunning)
-		validatorLineEdit.SetEnabled(!isRunning)
-
-		go func() {
-			errs := sserver.GetErrorOfType(socks5.ListenError{}, 3*time.Second)
-			if len(errs) != 0 {
-				isRunning := sserver.GetRunState()
-				StartButton.SetEnabled(!isRunning)
-				StopButton.SetEnabled(isRunning)
-				validatorLineEdit.SetEnabled(!isRunning)
-				for idx, listenErr := range errs {
-					if listenErr == nil {
-						continue
-					}
-					isRunning := sserver.GetRunState()
-					StartButton.SetEnabled(!isRunning)
-					StopButton.SetEnabled(isRunning)
-					validatorLineEdit.SetEnabled(!isRunning)
-
-					infoLineEdit.SetText(fmt.Sprintf("Listen err[%d]: %v", idx, listenErr))
-				}
-
-			}
-		}()
-
-	})
-	StopButton.ConnectClicked(func(bool) {
-		sserver.Stop()
-		isRunning := sserver.GetRunState()
-		StartButton.SetEnabled(!isRunning)
-		StopButton.SetEnabled(isRunning)
-		validatorLineEdit.SetEnabled(!isRunning)
-		infoLineEdit.SetText(fmt.Sprintf("stop[%v]: %v", isRunning, validatorLineEdit.Text()))
-	})
-
-	aboutButton.ConnectClicked(func(checked bool) {
-		widgets.QMessageBox_About(nil, "about qproxy", fmt.Sprintf("qproxy %s(%s)\nQt %s\n%s", verTag, verCommitHash, core.QtGlobal_qVersion(), strings.Replace(runtime.Version(), "go", "Go", -1)))
-	})
-
-	var validatorLayout = widgets.NewQGridLayout2()
-	validatorLayout.AddWidget2(validatorLabel, 0, 0, 0)
-	validatorLayout.AddWidget2(validatorLineEdit, 0, 1, 0)
-	validatorLayout.AddWidget2(StartButton, 1, 0, 0)
-	validatorLayout.AddWidget2(StopButton, 1, 1, 0)
-	validatorLayout.AddWidget2(infoLineEdit, 2, 0, 0)
-	validatorLayout.AddWidget2(aboutButton, 2, 1, 0)
-	validatorGroup.SetLayout(validatorLayout)
-
-	var layout = widgets.NewQGridLayout2()
-
-	layout.AddWidget2(validatorGroup, 1, 0, 0)
-
-	var window = NewHideWindow(nil, 0)
-	window.SetWindowTitle("代理")
-
-	openWinAction.ConnectTriggered(func(checked bool) {
-		window.Show()
-	})
-	closeWinAction.ConnectTriggered(func(bool) {
-		gui.QGuiApplication_SetQuitOnLastWindowClosed(true)
-		sysTray.Hide()
-		app.Quit()
-	})
-	sysTray.SetContextMenu(systrayMenu)
-	sysTray.ConnectActivated(func(reason widgets.QSystemTrayIcon__ActivationReason) {
-		switch reason {
-		case widgets.QSystemTrayIcon__DoubleClick:
-			window.Show()
-		}
-	})
-
-	var centralWidget = widgets.NewQWidget(window, 0)
-	centralWidget.SetLayout(layout)
-
-	window.SetCentralWidget(centralWidget)
+	window := NewProxyAppWindow(nil, 0).Init()
 	window.SetFixedHeight(150)
 	window.SetFixedWidth(250)
 	window.SetWindowIcon(gui.NewQIcon5(":/qml/icon.ico"))
+
 	window.Show()
+	window.ShowTray()
+
 	gui.QGuiApplication_SetQuitOnLastWindowClosed(false)
 	app.Exec()
 }
--- socks5server/sserver.go
+++ socks5server/sserver.go
@@ -2,92 +2,46 @@ package socks5server
 
 import (
 	"context"
-	"reflect"
-	"sync"
-	"time"
 
 	"github.com/Felamande/go-socks5"
-	"github.com/Workiva/go-datastructures/queue"
+	"github.com/therecipe/qt/core"
 )
 
 type Socks5Server struct {
+	core.QObject
 	lAddr    string
-	errQ     *queue.Queue
 	cancelFn func()
-	lock     sync.Mutex
-	state    bool
+	_        func(bool)        `signal:"runStateChange"`
+	_        func(interface{}) `signal:"receiveRunningError"`
 }
 
-func NewSocks5Server() *Socks5Server {
-	return &Socks5Server{
-		errQ:  queue.New(16),
-		state: false,
-	}
+func (s *Socks5Server) Init() *Socks5Server {
+	return s
 }
 
-func (s *Socks5Server) Stop() bool {
+func (s *Socks5Server) StopServer() {
 	if s.cancelFn != nil {
 		s.cancelFn()
-		s.changeState(false)
-		return true
+		s.RunStateChange(false)
+		return
 	}
-	return false
 }
 
-func (s *Socks5Server) Start(port string) error {
+func (s *Socks5Server) StartServer(port string) {
+
 	conf := &socks5.Config{}
 	server, _ := socks5.New(conf)
-
 	lAddr := ":" + port
 	s.lAddr = lAddr
 	ctx, cancelFn := context.WithCancel(context.Background())
 	s.cancelFn = cancelFn
 
-	s.changeState(true)
+	s.RunStateChange(true)
 	go func(ctxCancel context.Context) {
-		defer s.changeState(false)
+		defer s.RunStateChange(false)
 		if err := server.ListenAndServeWithCtx("tcp", lAddr, ctxCancel); err != nil {
-			s.errQ.Put(err)
+			s.ReceiveRunningError(err)
 			return
 		}
 	}(ctx)
-
-	return nil
-}
-
-func (s *Socks5Server) GetAllError(timeout time.Duration) []interface{} {
-	items, _ := s.errQ.Poll(16, timeout)
-	return items
-}
-
-func (s *Socks5Server) PeekError() interface{} {
-	items, _ := s.errQ.Peek()
-	return items
-}
-
-func (s *Socks5Server) GetErrorOfType(typ interface{}, timeout time.Duration) []interface{} {
-	retErrs := make([]interface{}, 4)
-	expectType := reflect.TypeOf(typ)
-	errs := s.GetAllError(timeout)
-	for _, err := range errs {
-		switch reflect.TypeOf(err) {
-		case expectType:
-			retErrs = append(retErrs, err)
-		default:
-			s.errQ.Put(err)
-		}
-	}
-	return retErrs
-}
-
-func (s *Socks5Server) changeState(isRunning bool) {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	s.state = isRunning
-}
-
-func (s *Socks5Server) GetRunState() bool {
-	s.lock.Lock()
-	defer s.lock.Unlock()
-	return s.state
 }
