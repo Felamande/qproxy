71882c3a2ffb898236e405e3432d82dd4c24562d
Felamande <elweinfancon@gmail.com>
2022-03-21 23:19:11 +0800

    fix logger

--- vmproxy/logger.go
+++ vmproxy/logger.go
@@ -14,41 +14,41 @@ func NewTangoChanLogger(logChan chan error) *tangoChanLogger {
 
 func (t *tangoChanLogger) Debugf(format string, v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[debug]"+format, v)
+		t.logChan <- fmt.Errorf("[debug]"+format, v...)
 	}()
 }
 func (t *tangoChanLogger) Debug(v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[debug]%v", v)
+		t.logChan <- fmt.Errorf("[debug]%v", v...)
 	}()
 }
 func (t *tangoChanLogger) Infof(format string, v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[info]"+format, v)
+		t.logChan <- fmt.Errorf("[info]"+format, v...)
 	}()
 }
 func (t *tangoChanLogger) Info(v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[info]%v", v)
+		t.logChan <- fmt.Errorf("[info]%v", v...)
 	}()
 }
 func (t *tangoChanLogger) Warnf(format string, v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[warn]"+format, v)
+		t.logChan <- fmt.Errorf("[warn]"+format, v...)
 	}()
 }
 func (t *tangoChanLogger) Warn(v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[warn]%v", v)
+		t.logChan <- fmt.Errorf("[warn]%v", v...)
 	}()
 }
 func (t *tangoChanLogger) Errorf(format string, v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[error]"+format, v)
+		t.logChan <- fmt.Errorf("[error]"+format, v...)
 	}()
 }
 func (t *tangoChanLogger) Error(v ...interface{}) {
 	go func() {
-		t.logChan <- fmt.Errorf("[error]%v", v)
+		t.logChan <- fmt.Errorf("[error]%v", v...)
 	}()
 }
